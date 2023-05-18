e9ca6e906bcd945637e7f00d3362cbbcb0364d9a
Alexandre Munaier <alexandremunaier027@gmail.com>
2023-05-18 16:27:50 -0300

    exercicio dom

---
+++ fundamentos/secao-4-dom-eventos-webStorage/script.js
@@ -0,0 +1,17 @@
+let header = document.getElementById('header-container').style.backgroundColor = 'green';
+
+let emergencyTasks = document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'red';
+let noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+
+let emergencyTaskH3 = document.querySelectorAll('.emergency-tasks h3');
+for (let index = 0; index < emergencyTaskH3.length; index += 1) {
+    emergencyTaskH3[index].style.backgroundColor = 'purple';
+};
+let noEmergencyTasksH3 = document.querySelectorAll('.no-emergency-tasks h3');
+for (let index = 0; index < noEmergencyTasksH3.length; index += 1) {
+    noEmergencyTasksH3[index].style.backgroundColor = 'black';
+};
+
+let footer = document.querySelector('#footer-container');
+footer.style.backgroundColor = 'green';
+footer.style.marginTop = '150px';
