04032e7313e19ae8806231da7529f1c378c1b464
Alexandre Munaier <alexandremunaier027@gmail.com>
2023-05-19 11:12:15 -0300

    mudei nada

--- fundamentos/secao-4-dom-eventos-webStorage/script.js
+++ fundamentos/secao-4-dom-eventos-webStorage/script.js
@@ -7,6 +7,7 @@ let emergencyTaskH3 = document.querySelectorAll('.emergency-tasks h3');
 for (let index = 0; index < emergencyTaskH3.length; index += 1) {
     emergencyTaskH3[index].style.backgroundColor = 'purple';
 };
+
 let noEmergencyTasksH3 = document.querySelectorAll('.no-emergency-tasks h3');
 for (let index = 0; index < noEmergencyTasksH3.length; index += 1) {
     noEmergencyTasksH3[index].style.backgroundColor = 'black';
@@ -15,3 +16,4 @@ for (let index = 0; index < noEmergencyTasksH3.length; index += 1) {
 let footer = document.querySelector('#footer-container');
 footer.style.backgroundColor = 'green';
 footer.style.marginTop = '150px';
+
